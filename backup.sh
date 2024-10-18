face55b6a80ad2a0cbd190ef9593e7e17e135ca0
Glastis <glastis@glastis.com>
2024-10-18 14:38:29 +0200

    feat: the host can now be specified. The default is still localhost

--- backup.sh
+++ backup.sh
@@ -21,11 +21,12 @@ function usage()
     echo -e "\t -o [OUT PATH]           Create path if it doesn't exist. If not used, will backup all in current working directory."
     echo -e "\t -a                      Backup all databases without asking for confirmation."
     echo -e "\t -f                      Backup information_schema and performance_schema, ignored by default."
+    echo -e "\t -H [MYSQL HOST]         Specify mysql host. Default is localhost."
     echo -e "\t -h                      Display this dialog."
     exit 0
 }
 
-while getopts "hfasu:p:d:o:" arg; do
+while getopts "hfaH:su:p:d:o:" arg; do
     case ${arg} in
         h)
             usage
@@ -35,6 +36,9 @@ while getopts "hfasu:p:d:o:" arg; do
             ;;
         a)
             BACKUP_ALL=1
+            ;;
+        H)
+            MYSQL_HOST=${OPTARG}
             ;;
         u)
             MYSQL_USERNAME=${OPTARG}
@@ -65,20 +69,28 @@ function show_options()
     echo "MYSQL_DATABASE: \"${MYSQL_DATABASE}\""
     echo "OUTPATH: \"${OUTPATH}\""
     echo "BACKUP_ALL: \"${BACKUP_ALL}\""
+    echo "MYSQL_HOST: \"${MYSQL_HOST}\""
 }
 
 function build_mysql_command()
 {
-    if [[ -n ${MYSQL_USERNAME} ]];              then MYSQL_COMMAND="${MYSQL_COMMAND} -u ${MYSQL_USERNAME}"; fi
-    if [[ -n ${MYSQL_PASSWORD} ]];              then MYSQL_COMMAND="${MYSQL_COMMAND} -p${MYSQL_PASSWORD}"; fi
-
-    if [[ -n ${MYSQL_USERNAME} ]];              then MYSQLDUMP_COMMAND="${MYSQLDUMP_COMMAND} -u ${MYSQL_USERNAME}"; fi
-    if [[ -n ${MYSQL_PASSWORD} ]];              then MYSQLDUMP_COMMAND="${MYSQLDUMP_COMMAND} -p${MYSQL_PASSWORD}"; fi
+    if [[ -n ${MYSQL_USERNAME} ]]; then
+        MYSQL_COMMAND="${MYSQL_COMMAND} -u ${MYSQL_USERNAME}"
+        MYSQLDUMP_COMMAND="${MYSQLDUMP_COMMAND} -u ${MYSQL_USERNAME}"
+    fi
+    if [[ -n ${MYSQL_PASSWORD} ]]; then
+        MYSQL_COMMAND="${MYSQL_COMMAND} -p${MYSQL_PASSWORD}"
+        MYSQLDUMP_COMMAND="${MYSQLDUMP_COMMAND} -p${MYSQL_PASSWORD}"
+    fi
+    if [[ -n ${MYSQL_HOST} ]]; then
+        MYSQL_COMMAND="${MYSQL_COMMAND} -h ${MYSQL_HOST}"
+        MYSQLDUMP_COMMAND="${MYSQLDUMP_COMMAND} -h ${MYSQL_HOST}"
+    fi
 }
 
 function get_all_databases()
 {
-    DB_LIST="$(echo "show databases;" | ${MYSQL_COMMAND})"
+    DB_LIST="$(echo "SHOW DATABASES;" | ${MYSQL_COMMAND})"
     if [[ ${?} == 1 ]]; then
         echo "Error while connecting to mysql."
         exit
@@ -114,9 +126,9 @@ function mysql_database_to_db_list()
 function backup_db_list()
 {
     ERR_LIST=""
-    if [[ -n ${OUTPATH} ]] ; then mkdir -p ${OUTPATH} ; fi
-    if [[ -n ${OUTPATH} ]] ; then OUTPATH="${OUTPATH}/" ; fi
-    for line in ${DB_LIST} ; do
+    if [[ -n ${OUTPATH} ]]; then mkdir -p ${OUTPATH}; fi
+    if [[ -n ${OUTPATH} ]]; then OUTPATH="${OUTPATH}/"; fi
+    for line in ${DB_LIST}; do
         echo "Backup ${line}..."
         ${MYSQLDUMP_COMMAND} ${line} > "${OUTPATH}${line}.sql"
         if [[ ${?} != 0 ]]; then
@@ -127,7 +139,7 @@ function backup_db_list()
             fi
         fi
     done
-    if [[ -n ${ERR_LIST} ]] ; then
+    if [[ -n ${ERR_LIST} ]]; then
         echo
         echo "Failed to back up following databases:"
         echo -e ${ERR_LIST}
@@ -136,7 +148,6 @@ function backup_db_list()
 
 function main()
 {
-#    show_options
     build_mysql_command
     get_all_databases
     confirm_backup_all
